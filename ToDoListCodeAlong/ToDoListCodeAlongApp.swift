4eae474cfb880bdc2290060f8ec9a8d5b954718f
Sonya <sonyahasthantra@gmail.com>
2023-06-13 08:22:03 -0700

    Initial Commit

---
+++ ToDoListCodeAlong/ToDoListCodeAlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListCodeAlongApp.swift
+//  ToDoListCodeAlong
+//
+//  Created by Dilan Hasthantra on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListCodeAlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
