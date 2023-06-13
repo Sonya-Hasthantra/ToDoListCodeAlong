0bc3d9ac68f5a7348ce4c0a771b6118306c5b744
Sonya <sonyahasthantra@gmail.com>
2023-06-13 09:53:38 -0700

    Completed Iteration 1 :)

--- ToDoListCodeAlong/ContentView.swift
+++ ToDoListCodeAlong/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var toDoItems: [ToDoItem] = []
+    @State private var showNewTask = false
     var body: some View {
         VStack {
             HStack {
@@ -19,16 +21,33 @@ struct ContentView: View {
                 Spacer()
                 //button
                 Button(action: {
-                                    
+                    self.showNewTask = true
                 }) {
                 Text("+")
                 }
             }
+            .padding()
+            Spacer()
+            
+            List{
+                ForEach(toDoItems){ toDoItem in
+                    if toDoItem.isImportant == true {
+                        Text("‼️" + toDoItem.title)
+                    } else {
+                        Text(toDoItem.title)
+                    }
+                    
+                }
+            }
         }
         .padding()
-        Spacer()
+        if showNewTask {
+            NewToDoView(title: "", isImportant: false,toDoItems: .constant([]),showNewTask: $showNewTask)
+                }
+        
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
